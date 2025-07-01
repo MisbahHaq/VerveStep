7d46c5f5d2dad60cf18ea277197472027e63cfe9
Misbah Haque <misbahmuneer2003@gmail.com>
2025-07-01 15:38:05 +0500

    Update

--- script.js
+++ script.js
@@ -52,3 +52,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+
+const images = document.querySelectorAll('.hover-img');
+
+images.forEach(img => {
+    const originalSrc = img.src;
+    const hoverSrc = img.getAttribute('data-hover');
+
+    img.addEventListener('mouseenter', () => {
+        img.src = hoverSrc;
+    });
+
+    img.addEventListener('mouseleave', () => {
+        img.src = originalSrc;
+    });
+});
+
